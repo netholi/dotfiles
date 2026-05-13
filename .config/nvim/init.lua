56bdda708965f8623221a9728aff88c019d1d504
Anand Janardhanan <ajkadakkal@gmail.com>
2026-05-13 19:53:42 +0530

    add mini and status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,9 +3,11 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("config.status")
+
+
 require("plugins.lsp")
-
---require("plugins.treesitter")
+require("plugins.treesitter")
 require("plugins.neo-tree")
-
 require("plugins.completion")
+require("plugins.mini")
